f35e681163fa10bc8b5b6dd2762f0fd42d5540f7
yijie.qj <yijie.qj@alibaba-inc.com>
2025-03-18 16:18:30 +0800

    resource/alicloud_click_house_db_cluster: update example module for allocating public connection. resource/alicloud_click_house_db_cluster: update example module for using resource group. resource/alicloud_click_house_db_cluster: update example module for using cold storage.

--- examples/complete/tfvars/01-update.tfvars
+++ examples/complete/tfvars/01-update.tfvars
@@ -2,11 +2,13 @@
 db_cluster_description = "tf-test-update-clickhouse"
 db_cluster_access_white_list = [{
   db_cluster_ip_array_name = "test"
-  security_ip_list = "127.0.0.2"
-},{
+  security_ip_list         = "127.0.0.2"
+  }, {
   db_cluster_ip_array_name = "test2"
-  security_ip_list = "127.0.0.0,127.1.1.1"
+  security_ip_list         = "127.0.0.0,127.1.1.1"
 }]
+allocate_public_connection = true
+cold_storage               = "ENABLE"
 #alicloud_click_house_account
 account_description = "tf-test-update-clickhouse-account"
 account_password    = "Tf-updatetestpwd"
--- main.tf
+++ main.tf
@@ -14,21 +14,24 @@ resource "alicloud_click_house_db_cluster" "cluster" {
   dynamic "multi_zone_vswitch_list" {
     for_each = var.multi_zone_vswitch_list
     content {
-      zone_id = multi_zone_vswitch_list.value.zone_id
+      zone_id    = multi_zone_vswitch_list.value.zone_id
       vswitch_id = multi_zone_vswitch_list.value.vswitch_id
     }
   }
 
-  db_cluster_network_type = "vpc"
-  db_cluster_description  = var.db_cluster_description
-  db_node_group_count     = var.db_node_group_count
-  payment_type            = var.payment_type
-  renewal_status          = local.this_cluster_renewal_status
-  period                  = local.this_cluster_period
-  used_time               = local.this_cluster_used_time
-  db_node_storage         = var.db_node_storage
-  storage_type            = var.storage_type
-  vswitch_id              = var.vswitch_id
+  db_cluster_network_type    = "vpc"
+  db_cluster_description     = var.db_cluster_description
+  db_node_group_count        = var.db_node_group_count
+  payment_type               = var.payment_type
+  renewal_status             = local.this_cluster_renewal_status
+  period                     = local.this_cluster_period
+  used_time                  = local.this_cluster_used_time
+  db_node_storage            = var.db_node_storage
+  storage_type               = var.storage_type
+  vswitch_id                 = var.vswitch_id
+  resource_group_id          = var.resource_group_id
+  allocate_public_connection = var.allocate_public_connection
+  cold_storage               = var.cold_storage
 }
 
 resource "alicloud_click_house_account" "account" {
